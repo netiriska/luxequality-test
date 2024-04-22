60651a233346ff6f1fc8a6cbd2467ec3b8079721
netiriska <i.sidik@ukr.net>
2024-04-22 13:12:27 +0300

    Main col row

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 export default function Main() {
-  return <p>Main text</p>;
+  return (
+    <Container fluid={true}>
+      <Row>
+        <Col>Options</Col>
+        <Col xs={8}>Map</Col>
+        <Col>Chosen options</Col>
+      </Row>
+    </Container>
+  );
 }
