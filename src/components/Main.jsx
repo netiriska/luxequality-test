ff8e5fa7a099cce3284678e1f994d1c5a709c5c8
netiriska <i.sidik@ukr.net>
2024-04-22 17:09:37 +0300

    cluster icon

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,26 @@
 import { Col, Container, Row } from "react-bootstrap";
-import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
-import geoIcon from "../icons/geo-icon.svg"
+import { Icon, divIcon, point } from "leaflet";
+import geoIcon from "../icons/geo-icon.svg";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 export default function Main() {
   const markers = [
     {
       key: "marker1",
       geocode: [50.4503206, 30.52413],
-      popup: "1",
+      popup: "Apartment 1",
     },
     {
       key: "marker2",
       geocode: [50.4501132, 30.5341351],
-      popup: "2",
+      popup: "Apartment 2",
     },
     {
       key: "marker3",
       geocode: [50.4505436, 30.5441161],
-      popup: "3",
+      popup: "Apartment 3",
     },
   ];
 
@@ -27,6 +28,14 @@ export default function Main() {
     iconUrl: geoIcon,
     iconSize: [38, 38],
   });
+
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true),
+    });
+  };
 
   return (
     <Container fluid={true}>
@@ -38,9 +47,21 @@ export default function Main() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {markers.map((marker) => (
-              <Marker position={marker.geocode} icon={customIcon}></Marker>
-            ))}
+
+            <MarkerClusterGroup
+              chunkedLoading
+              iconCreateFunction={createCustomClusterIcon}
+            >
+              {markers.map((marker) => (
+                <Marker
+                  key={marker.key}
+                  position={marker.geocode}
+                  icon={customIcon}
+                >
+                  <Popup>{marker.popup}</Popup>
+                </Marker>
+              ))}
+            </MarkerClusterGroup>
           </MapContainer>
         </Col>
         <Col>Chosen options</Col>
